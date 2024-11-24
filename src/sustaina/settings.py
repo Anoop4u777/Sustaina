71da33e6ab5d641d826e017b02ccdf91b60a6ad3
Anoop Krishnan Ramachandran <anoop4u777@gmail.com>
2024-11-24 12:59:15 -0500

    Changes in requirements and settings

--- src/sustaina/settings.py
+++ src/sustaina/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 
+from decouple import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-nbm#dd_muwnxo-r5qw3^+k*!33o%7$w7e3b#t%=e7xn01k*gac'
+SECRET_KEY = config("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DJANGO_DEBUG", cast=bool)
 
 ALLOWED_HOSTS = ['.railway.app']
 
