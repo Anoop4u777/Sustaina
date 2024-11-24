77cf6b5b40f4782932871c580217e7f2a25bf507
Anoop Krishnan Ramachandran <anoop4u777@gmail.com>
2024-11-24 12:18:33 -0500

    Changes: About page and some more changes in views

--- src/sustaina/settings.py
+++ src/sustaina/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = 'django-insecure-nbm#dd_muwnxo-r5qw3^+k*!33o%7$w7e3b#t%=e7xn01k*gac
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.railway.app']
+
+if DEBUG:
+    ALLOWED_HOSTS += ["127.0.0.1", "localhost"]
 
 
 # Application definition
--- src/sustaina/views.py
+++ src/sustaina/views.py
@@ -6,7 +6,23 @@ from visits.models import PageVisit
 
 this_dir = pathlib.Path(__file__).resolve().parent
 
-def home_page(request, *args, **kwargs):
-    queryset = PageVisit.objects.all().count()
+def home_view(request, *args, **kwargs):
+    return about_view(request, *args, **kwargs)
+
+def about_view(request, *args, **kwargs):
+    queryset = PageVisit.objects.all()
+    page_queryset = PageVisit.objects.filter(path=request.path)
+    try:
+        percent = (page_queryset.count() * 100.0 / queryset.count())
+    except:
+        percent = 0
+
     PageVisit.objects.create(path=request.path)
-    return render(request, "home.html", {"page_title":"Hai test title", "queryset": queryset})
+
+    my_context = {
+        "page_title":"Hai test title",
+        "page_visit_count": page_queryset.count(),
+        "percent" : percent,
+        "total_visit": queryset.count()
+        }
+    return render(request, "about.html", context=my_context)
