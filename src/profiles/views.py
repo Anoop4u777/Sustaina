7cb82c91d17a9f025b3b1ce74f2519a432651928
Anoop Krishnan Ramachandran <anoop4u777@gmail.com>
2024-11-26 00:39:11 -0500

    Changes: Made more changes with perms

--- src/profiles/views.py
+++ src/profiles/views.py
@@ -7,13 +7,19 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 
+@login_required
+def profile_list_view(request, *args, **kwargs):
+    context = {
+        "object_list": User.objects.filter(is_active=True)
+    }
+    return render(request, 'profiles/list.html', context)
+
 # Create your views here.
 @login_required
-def profile_view(request, *args, **kwargs):
-
-    # To see the permissions.
-    print("auth.view_user", request.user.has_perm("auth.view_user"))
-    print("visits.view_pagevisit", request.user.has_perm("visits.view_pagevisit"))
+def profile_detail_view(request, *args, **kwargs):
+    # # To see the permissions.
+    # print("auth.view_user", request.user.has_perm("auth.view_user"))
+    # print("visits.view_pagevisit", request.user.has_perm("visits.view_pagevisit"))
 
     # Other options are.
     # refer : https://docs.djangoproject.com/en/5.1/topics/auth/default/#default-permissions
@@ -23,4 +29,11 @@ def profile_view(request, *args, **kwargs):
     # <app_label>.change_<model_name>
 
     user_object = get_object_or_404(User, username=kwargs['username'])
-    return HttpResponse(f"Hello there {kwargs['username']} - {user_object.id}")
+    # return HttpResponse(f"Hello there {kwargs['username']} - {user_object.id}")
+    owner = user_object == request.user
+    context = {
+        "instance" : user_object,
+        "owner": owner
+
+    }
+    return render(request, 'profiles/details.html', context)
